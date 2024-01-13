147a457d258099b4348c5f1685ab2aae128d6366
Toha Islam <islamtoha710@gmail.com>
2024-01-13 14:06:06 +0600

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
-        <Button className="w-[500px]" variant="outline">
-          Click
-        </Button>
+        <div className="w-96 border border-red-500 p-10">
+          <Button>Click</Button>
+        </div>
       </div>
     </Container>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,27 +1,60 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
 import cn from "../../utils/cn";
 
-const Button = ({
-  className,
-  outline,
-}: {
-  className: string;
-  outline: string;
-}) => {
-  return (
-    <div>
-      <button
-        className={cn(
-          "btn-ghost",
-          {
-            "border-purple-500 bg-opacity-10": outline,
-          },
-          className
-        )}
-      >
-        Click
-      </button>
-    </div>
-  );
+type TRef = HTMLButtonElement;
+
+type TButtonOptions = {
+  variant?: TVariant;
 };
 
+type TVariant = "solid" | "ghost" | "outline";
+
+type TButton = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  TButtonOptions;
+
+const Button = forwardRef<TRef, TButton>(
+  ({ className, variant = "solid", ...rest }, ref) => {
+    const getVariant = (variant: TVariant) => {
+      switch (variant) {
+        case "outline":
+          return "btn-outline";
+        case "ghost":
+          return "btn-ghost";
+
+        default:
+          return "btn-solid";
+      }
+    };
+
+    return (
+      <div>
+        <button
+          {...rest}
+          ref={ref}
+          className={cn(getVariant(variant), className)}
+        >
+          Click
+        </button>
+      </div>
+    );
+  }
+);
+
 export default Button;
+
+// type TRef = HTMLButtonElement;
+
+// type TButtonOptions = {
+//   variant?: TVariant;
+// };
+
+// type TVariant = "solid" | "ghost" | "outline";
+
+// type TButton = DetailedHTMLProps<
+//   ButtonHTMLAttributes<HTMLButtonElement>,
+//   HTMLButtonElement
+// > &
+//   TButtonOptions;
